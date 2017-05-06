44094716df7cea8fa8e15b9fd6ef5b8e73fc15cc
eunicebes <eunicebes@gmail.com>
2017-05-06 21:11:54 +0800

    Check if good store

--- review_crawler.py
+++ review_crawler.py
@@ -13,16 +13,16 @@ SELLER_ID = str(sys.argv[1])
 FIRST_PAGE_URL = SITE + SELLER_ID + SITE_SEED + '1'
 
 def get_seller_name():
-	html_text = session.get(FIRST_PAGE_URL, verify = True)
-	soup = BeautifulSoup(html_text.text.encode('utf-8'), 'html.parser')
-	name = soup.find('title').getText().split('-')[0]
+	# html_text = session.get(FIRST_PAGE_URL, verify = True)
+	# soup = BeautifulSoup(html_text.text.encode('utf-8'), 'html.parser')
+	name = FIRST_PAGE_SOUP.find('title').getText().split('-')[0]
 
 	return name
 
 def get_last_page():
-	html_text = session.get(FIRST_PAGE_URL, verify = True)
-	soup = BeautifulSoup(html_text.text.encode('utf-8'), 'html.parser')
-	class_found = soup.find('span', {'class': 'pagenum'}).getText()
+	# html_text = session.get(FIRST_PAGE_URL, verify = True)
+	# soup = BeautifulSoup(html_text.text.encode('utf-8'), 'html.parser')
+	class_found = FIRST_PAGE_SOUP.find('span', {'class': 'pagenum'}).getText()
 	last_page_num = class_found.split(' ')[5]
 	
 	return last_page_num
@@ -36,10 +36,19 @@ def get_pages_url():
 
 	return pages
 
+def check_good_store():
+	class_found = FIRST_PAGE_SOUP.find('div', {'class': 'mainpane'})
+	good_pic_alt = class_found.find('img')['alt']
+	if good_pic_alt == '優良商店':
+		return True
+	else:
+		return False
+
 def get_comment():
 	# seller_id = 'ryoushoku'
 	seller_name = get_seller_name()
 	pages = get_pages_url()
+	good_store = check_good_store()
 
 	comments_ary = []
 	for page in pages:
@@ -48,7 +57,10 @@ def get_comment():
 		list_table_comment = soup.find('table', {'class': 'listtable'}).find('tbody')
 		comments = list_table_comment.find_all('tr')
 		for item in comments:
-			score = item.find('em', {'class': 'store'}).getText()
+			if good_store:
+				score = item.find('em', {'class': 'store-sup'}).getText()
+			else:
+				score = item.find('em', {'class': 'store'}).getText()
 			content = item.find('dl', {'class': 'comment'}).find('dd').getText()
 			buyer_id = item.find_all('td')[-1].getText()
 			date = item.find('th').getText()
@@ -61,7 +73,7 @@ def get_comment():
 			}
 			
 			comments_ary.append(comment)
-
+			
 			print (buyer_id)			
 	
 	comments_per_shop = {
@@ -78,6 +90,9 @@ def get_comment():
 
 if __name__ == "__main__":
     session = requests.session()
+
+    FIRST_PAGE_HTML = session.get(FIRST_PAGE_URL, verify = True)
+    FIRST_PAGE_SOUP = BeautifulSoup(FIRST_PAGE_HTML.text.encode('utf-8'), 'html.parser')
 
     # get and store all buyer reviews
     get_comment()
